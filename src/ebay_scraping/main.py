aa23d84bac517d1a19a92901082241f51e7b8d15
MR-RITZY <mrboffin01@gmail.com>
2025-11-18 17:05:20 +0100

    Initial Commit --Ebay Challenge Code for API Activation

---
+++ src/ebay_scraping/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from hashlib import sha256
+
+from ebay_scraping.config import settings
+from ebay_scraping.schema import EbayChallengeResponse
+
+app = FastAPI()
+
+
+@app.get("/ebay/callback", response_model=EbayChallengeResponse)
+def get_ebay_challenge_code(challenge_code: str):
+    endcoded = f"{challenge_code}{settings.EBAY_VERIFICATION_TOKEN}{settings.BASE_URL}".encode()
+    challenge_response = sha256(endcoded, usedforsecurity=True).hexdigest()
+
+    return {"challengeResponse": challenge_response}
