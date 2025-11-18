b8c1fb286c8e09860073f65672e1bba3d2006c2b
MR-RITZY <mrboffin01@gmail.com>
2025-11-18 19:05:33 +0100

    Update add root

--- src/ebay_scraping/main.py
+++ src/ebay_scraping/main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 from hashlib import sha256
+from fastapi.responses import RedirectResponse
 
 from ebay_scraping.config import settings
 from ebay_scraping.schema import EbayChallengeResponse
@@ -8,8 +9,15 @@ app = FastAPI()
 
 
 @app.get("/ebay/callback", response_model=EbayChallengeResponse)
-def get_ebay_challenge_code(challenge_code: str):
+async def get_ebay_challenge_code(challenge_code: str):
     endcoded = f"{challenge_code}{settings.EBAY_VERIFICATION_TOKEN}{settings.BASE_URL}".encode()
     challenge_response = sha256(endcoded, usedforsecurity=True).hexdigest()
 
     return {"challengeResponse": challenge_response}
+
+
+@app.get("/")
+async def get_root():
+    return {
+        "message": "Welcome!"
+    }
